84684d9e350aa8759657ebef1735151d4eb63ea5
sondt <sondt.vn@gmail.com>
2021-04-05 13:30:55 +0700

    Update Branch test

---
+++ TodoApi/Model/People.cs
@@ -0,0 +1,13 @@
+namespace TodoApi.Model
+{
+    public class People
+    {
+        public int ID { get; set; }
+        
+        
+        public string Name { get; set; }
+        
+        public int Age { get; set; }        
+        
+    }
+}
